55e108a15cd7303fc0de2c2e0947b63d11654eac
psu6810110164 <6810110164@psu.ac.th>
2025-12-19 11:25:44 +0700

    Create User Resource and Hashing Password & Admin Seeding

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserRole } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+export class UsersService implements OnModuleInit {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) { }
+
+  async onModuleInit() {
+    // Auto-create Admin user for testing
+
+    const admin = await this.findOneByEmail('admin@bookstore.com');
+    if (!admin) {
+      console.log('Seeding Admin User...');
+      await this.create({
+        email: 'admin@bookstore.com',
+        password: 'adminpassword',
+        role: UserRole.ADMIN
+      } as any);
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async create(createUserDto: CreateUserDto) {
+    // Hash password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
+
+    const user = this.userRepository.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+    return this.userRepository.save(user);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findAll() {
+    return this.userRepository.find();
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOne(id: string) {
+    return this.userRepository.findOneBy({ id });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      const salt = await bcrypt.genSalt();
+      updateUserDto.password = await bcrypt.hash(
+        updateUserDto.password,
+        salt,
+      );
+    }
+
+    await this.userRepository.update(id, updateUserDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: string) {
+    return this.userRepository.delete(id);
+  }
+
+  async findOneByEmail(email: string) {
+    return this.userRepository.findOneBy({ email });
   }
 }
