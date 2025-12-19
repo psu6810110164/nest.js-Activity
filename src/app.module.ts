35b46de1c0301666bfc141404537ea31a4cafc3c
psu6810110164 <6810110164@psu.ac.th>
2025-12-19 10:48:13 +0700

    Setup Environment Variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,39 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookCategoryModule } from './book-category/book-category.module';
 import { BookCategory } from './book-category/entities/book-category.entity';
 import { BookModule } from './book/book.module';
 import { Book } from './book/entities/book.entity';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'admin',
-      password: 'password123',
-      database: 'bookstore_dev',
-      entities: [Book, BookCategory], // เราจะเพิ่ม Entities ที่นี่ในภายหลัง
-      synchronize: true, // สร้าง Table อัตโนมัติ (ใช้สำหรับ Dev เท่านั้น)
+    ConfigModule.forRoot({ 
+      isGlobal: true,
+      envFilePath: '.env',
     }),
+
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],      // บอกว่า Module นี้ต้องใช้ ConfigModule
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'), // ใช้ ConfigService ดึงค่า แทน process.env
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],      // Inject ConfigService เข้ามาใน Factory
+    }),
+    
     BookCategoryModule,
     BookModule,
+    UsersModule,
   ],
 })
 export class AppModule { }
